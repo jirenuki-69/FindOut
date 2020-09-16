864215c7123f36c62691c777e39c9121099e3b0b
jirenuki-69 <mfuentesmodelo@gmail.com>
2020-09-16 15:31:34 -0500

    Inicio de proyecto

---
+++ lib/constants/constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+//Strings
+const String imgPath = "lib/assets/img";
+const String iconPath = "lib/assets/icons";
+
+//Colores
+final primaryColor = Color(0xFFFF256C);
+final lightPrimaryColor = Color(0XFFFED1E6);
+final LinearGradient gradient = LinearGradient(
+  colors: <Color>[
+    primaryColor,
+    lightPrimaryColor,
+  ],
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  stops: [0.5, 1.0],
+);
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:findout/constants/constants.dart';
+import 'package:findout/src/screens/Login/login.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: primaryColor,
+        fontFamily: 'Poppins',
+      ),
+      home: LoginPage(),
+    );
+  }
+}
